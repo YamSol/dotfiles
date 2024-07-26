b3aff82b0a58da5dbd18d70d4432d9ba204f692c
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2024-07-26 03:22:22 -0300

    chore(fish): just ordenate options

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,17 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
 end
 
-# LOAD ASDF 
+# ASDF 
 source ~/.asdf/asdf.fish
 
+#ALIAS
 alias fd=fdfind
 alias sai="sudo apt install"
 alias sau="sudo apt update"
 
 # PATH APPEND
-#contains ~/.config/emacs/bin/ fish_user_paths; or set -Ua fish_user_paths ~/.config/emacs/bin/
-fish_add_path ~/.config/emacs/bin
 fish_add_path -m ~/.local/bin
 fish_add_path -m ~/bin
+fish_add_path ~/.config/emacs/bin
 fish_add_path ~/.config/rofi/scripts
+
